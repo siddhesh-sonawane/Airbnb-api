781adb6a37e9f233381c5d6a54b6507e883e76d2
siddheshsonawane-lang <siddheshbalkrushnasonawane@gmail.com>
2026-03-29 22:36:45 +0530

    .

--- src/main/java/com/Siddhesh/Airbnb/Repository/UserRepository.java
+++ src/main/java/com/Siddhesh/Airbnb/Repository/UserRepository.java
@@ -3,5 +3,11 @@ package com.Siddhesh.Airbnb.Repository;
 import com.Siddhesh.Airbnb.Model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
+
+    Optional<User> findByUsername(String username);
 }
